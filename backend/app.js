e776e84f57544bf246c78c09c4659c371e48b20c
Tushar Kumar <tusharkr43@gmail.com>
2025-02-23 13:31:11 +0530

    user

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,9 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
-// const userRoutes = require('./routes/user.routes');
+const userRoutes = require('./routes/user.routes');
 // const captainRoutes = require('./routes/captain.routes');
 // const mapsRoutes = require('./routes/maps.routes');
 // const rideRoutes = require('./routes/ride.routes');
@@ -15,15 +15,14 @@ connectToDb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-
+app.use(cookieParser());
 
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// app.use('/users', userRoutes);
+app.use('/users', userRoutes);
 // app.use('/captains', captainRoutes);
 // app.use('/maps', mapsRoutes);
 // app.use('/rides', rideRoutes);
