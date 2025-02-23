5457064ea0f5968e5092d6449d8984dc07704377
Tushar Kumar <tusharkr43@gmail.com>
2025-02-23 19:31:32 +0530

    socket for the real world chat , ride creation

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,8 @@ const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
-// const mapsRoutes = require('./routes/maps.routes');
-// const rideRoutes = require('./routes/ride.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
 
@@ -24,8 +24,8 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
-// app.use('/maps', mapsRoutes);
-// app.use('/rides', rideRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
 
 
 
